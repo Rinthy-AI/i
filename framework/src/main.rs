81631cc2392a95898309b18d50a139cb2a0b16bd
Logan Boyd <logan.boyd@protonmail.com>
2025-01-15 18:38:01 -0800

    RESET ME. Update framework/src/main.rs

--- framework/src/main.rs
+++ framework/src/main.rs
@@ -4,15 +4,18 @@ fn main() -> Result<(), String> {
     let h = i("ik*kj~ijk")?;
     let a = i("+ijk~ij")?;
 
-    // this should work EDIT: apparently this won't ever work in stable Rust :(((
-    //let mm = a(h);
+    let mm = h.chain(&a);
 
-    let mm = h.chain(&a); // make this work
-
-    // this should work
-    //let x = Array { data: vec![0., -1., 1., 0.], shape: vec![2, 2] };
-    //let y = Array { data: vec![1., 2., 3., 4.], shape: vec![2, 2] };
-    //println!("{:#?}", mm(&x, &y));
+    // todo: make this work
+    let x = Array {
+        data: vec![0., -1., 1., 0.],
+        shape: vec![2, 2],
+    };
+    let y = Array {
+        data: vec![1., 2., 3., 4.],
+        shape: vec![2, 2],
+    };
+    println!("{:#?}", mm(&x, &y));
 
     Ok(())
 }
