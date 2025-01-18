517c82a69a3d6ff506b43c61565c0eb2e8df01d9
Logan Boyd <logan.boyd@protonmail.com>
2025-01-17 20:34:40 -0800

    Remove unused `backend::rust::Array` type
    Also clean up example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,87 +1,24 @@
-use compiler::backend::rust::Array;
 use macros::i;
 
 fn main() {
     // matrix multiplication kernel
     let mm = i!(
-        ij+ij~ij
+        m: ik*kj~ijk
+        a: +ijk~ij
+        m.a
     );
 
-    let mut x = Array::new(vec![2, 2], 0.);
-    let mut y = Array::new(vec![2, 2], 0.);
-
-    // 90-degree counterclockwise rotation matrix
-    x[&[0, 0]] = 1.;
-    x[&[0, 1]] = 1.;
-    x[&[1, 0]] = 1.;
-    x[&[1, 1]] = 1.;
-
-    // some other matrix
-    y[&[0, 0]] = 1.;
-    y[&[0, 1]] = 2.;
-    y[&[1, 0]] = 3.;
-    y[&[1, 1]] = 4.;
+    // i operates on contiguous arrays
+    let x = vec![0., -1., 1., 0.];
+    let y = vec![1., 2., 3., 4.];
+    let mut out = vec![0., 0., 0., 0.];
 
     // matmul
     // |0 -1||1 2| = |0 0| + |-3 -4| = |-3 -4|
     // |1  0||3 4|   |1 2|   | 0  0|   | 1  2|
-    //let c = a(p(x, y));
 
-    let c = mm(x, y);
+    // pass dims with data vecs
+    let c = mm(&x, 2, 2, &y, 2, 2, &mut out, 2, 2);
 
-    println!("{:#?}", c);
+    println!("{:#?}", out);
 }
-
-//use std::ops::{Index, IndexMut};
-//
-//#[derive(Debug)]
-//struct Array {
-//    data: Vec<f32>,
-//    shape: Vec<usize>, // Dimensions of the array
-//}
-//
-//impl Array {
-//    fn new(shape: Vec<usize>, initial_value: f32) -> Self {
-//        let size = shape.iter().product();
-//        Array {
-//            data: vec![initial_value; size],
-//            shape,
-//        }
-//    }
-//
-//    // Define your affine transform to compute 1-D index from N-D indices
-//    fn affine_transform(&self, nd_indices: &[usize]) -> Option<usize> {
-//        if nd_indices.len() != self.shape.len() {
-//            return None;
-//        }
-//
-//        // Example affine transform: here you can implement your actual logic
-//        let mut idx = 0;
-//        for (i, &dim_index) in nd_indices.iter().enumerate() {
-//            if dim_index >= self.shape[i] {
-//                return None;
-//            }
-//            idx = idx * self.shape[i] + dim_index; // Simple affine transformation
-//        }
-//
-//        Some(idx)
-//    }
-//}
-//
-//// Implement immutable indexing (using `[]`)
-//impl Index<&[usize]> for Array {
-//    type Output = f32;
-//
-//    fn index(&self, indices: &[usize]) -> &Self::Output {
-//        let idx = self.affine_transform(indices).expect("Invalid index");
-//        &self.data[idx]
-//    }
-//}
-//
-//// Implement mutable indexing (using `[]`)
-//impl IndexMut<&[usize]> for Array {
-//    fn index_mut(&mut self, indices: &[usize]) -> &mut Self::Output {
-//        let idx = self.affine_transform(indices).expect("Invalid index");
-//        &mut self.data[idx]
-//    }
-//}
