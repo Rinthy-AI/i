927174b043fc1857c2e9e15d818d6c6f69754567
Logan Boyd <logan.boyd@protonmail.com>
2025-04-27 11:47:09 -0700

    Add `itensor::Component` type

--- itensor/example.py
+++ itensor/example.py
@@ -1,7 +1,10 @@
-from itensor import Tensor
+from itensor import Tensor, Component as i
 
 print(Tensor([1]))
 print(Tensor([1, 2]))
 print(Tensor([[1], [2]]))
 print(Tensor([[1, 2]]))
 print(Tensor([[1, 2], [3, 4]]))
+
+print(i("ik*kj~ijk"))
+
