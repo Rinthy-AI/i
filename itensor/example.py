5a1d0df143482c6fe5a218ef011b68b0fc1cbb50
Logan Boyd <logan.boyd@protonmail.com>
2025-05-19 23:32:06 -0700

    Update itensor example.py

--- itensor/example.py
+++ itensor/example.py
@@ -6,5 +6,15 @@ print(Tensor([[1], [2]]))
 print(Tensor([[1, 2]]))
 print(Tensor([[1, 2], [3, 4]]))
 
-print(i("ik*kj~ijk"))
+#print(i("ik*kj~ijk"))
+out = i("""
+h: ik*kj~ijk
+a: +ijk~ij
+h.a
+""").exec(
+    Tensor([[1, 2], [3, 4]]),
+    Tensor([[1, 2], [3, 4]]),
+)
+
+print(out)
 
